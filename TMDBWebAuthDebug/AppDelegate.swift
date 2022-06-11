f476b91a12481639f84c8852b0f54342cf9c7767
Mitch Salcido <salcidoconsulting@mac.com>
2022-06-11 15:43:09 -0700

    initial commit

--- TMDBWebAuthDebug/AppDelegate.swift
+++ TMDBWebAuthDebug/AppDelegate.swift
@@ -11,6 +11,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        print("AppDelegate: app open options")
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- TMDBWebAuthDebug/ViewController.swift
+++ TMDBWebAuthDebug/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    let apikey = "ea5530947a898e99eb0d439c1f0cb9ab"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tmdbWebAuthLoginButtonPressed(_ sender: Any) {
+        print("tmdbWebAuthLoginButtonPressed")
+        
+        getRequestToken()
+    }
 }
 
+extension ViewController {
+    
+    func getRequestToken() {
+        
+        let urlStr = "https://api.themoviedb.org/3/authentication/token/new?api_key=\(apikey)"
+        guard let url = URL(string: urlStr) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            
+            let obj = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+            guard let requestToken = obj["request_token"] as? String else {
+                return
+            }
+            print("requestToken: \(requestToken)")
+            
+            let urlStr = "https://www.themoviedb.org/authenticate/" + requestToken + "?redirect_to=tmdbwebauthdebug:authenticate"
+            guard let url = URL(string: urlStr) else {
+                return
+            }
+            print("URL: \(url)")
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        task.resume()
+    }
+}
